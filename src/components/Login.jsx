591d2125edd28e7979ac6122afea0e2ac39ab46f
Pratik Devnal <79634014+pratikdevnal@users.noreply.github.com>
2024-05-03 18:15:13 +0530

    Refractored Code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import SocailLogin from "./SocailLogin";
 const Login = ({ changeLogin }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [sucessMessage, setSuccessMessage] = useState("");
-  const handleLogin = async () => {
+  // const [message, setMessage] = useState("");
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    for (const [key, value] of Object.entries(formData)) {
+      if (!value) {
+        console.log(key);
+        // setMessage(`please fill ${key} field`);
+        return;
+      } else {
+        // setMessage(`Form Submited Successfully`);
+      }
+    }
     try {
       const response = await fetch("http://127.0.0.1:5000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username: username, password: password }),
+        body: JSON.stringify({
+          username: formData.username,
+          password: formData.password,
+        }),
       });
 
       const data = await response.json();
@@ -34,44 +52,58 @@ const Login = ({ changeLogin }) => {
       console.error("Login failed:", error.message);
     }
   };
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {errorMessage ? (
-        <p className="" id="error-message">
-          {errorMessage}
-        </p>
-      ) : (
-        <></>
-      )}
-      {sucessMessage ? (
-        <p className="" id="success-message">
-          {sucessMessage}
-        </p>
-      ) : (
-        <></>
-      )}
-      <button onClick={handleLogin}>Login</button>
+    <div className="">
+      <form
+        onSubmit={onSubmitHandler}
+        className="form login-container"
+        autoComplete="off"
+      >
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={formData.username}
+          onChange={onChangeHandler}
+          name="username"
+          required="required"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={onChangeHandler}
+          name="password"
+          required="required"
+        />
+        {errorMessage ? (
+          <p className="" id="error-message">
+            {errorMessage}
+          </p>
+        ) : (
+          <></>
+        )}
+        {sucessMessage ? (
+          <p className="" id="success-message">
+            {sucessMessage}
+          </p>
+        ) : (
+          <></>
+        )}
+        <button type="submit">Login</button>
 
-      <p>
-        Don't have an account?{" "}
-        <span onClick={() => changeLogin(false)}>Signup</span>
-      </p>
+        <p>
+          Don't have an account?{" "}
+          <span onClick={() => changeLogin(false)}>Signup</span>
+        </p>
 
-      <SocailLogin />
+        <SocailLogin />
+      </form>
     </div>
   );
 };
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import SocailLogin from "./SocailLogin";
 const Signup = ({ changeLogin }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [sucessMessage, setSuccessMessage] = useState("");
-  const handleSignup = async () => {
-    if (!username || !password) {
-      setErrorMessage("Fill all required fields");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  // const [message, setMessage] = useState("");
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    for (const [key, value] of Object.entries(formData)) {
+      if (!value) {
+        console.log(key);
+        // setMessage(`please fill ${key} field`);
+        return;
+      } else {
+        // setMessage(`Form Submited Successfully`);
+      }
     }
     try {
       const response = await fetch("http://127.0.0.1:5000/signup", {
@@ -15,7 +24,10 @@ const Signup = ({ changeLogin }) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username: username, password: password }),
+        body: JSON.stringify({
+          username: formData.username,
+          password: formData.password,
+        }),
       });
 
       const data = await response.json();
@@ -34,43 +46,69 @@ const Signup = ({ changeLogin }) => {
       console.error("Login failed:", error.message);
     }
   };
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [sucessMessage, setSuccessMessage] = useState("");
+
+  // const handleSignup = async () => {};
 
   return (
-    <div className="signup-container">
-      <h2>Signup</h2>
-      <input
-        type="text"
-        placeholder="Email"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Create password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input type="password" placeholder="Confirm password" />
-      {errorMessage ? (
-        <p className="" id="error-message">
-          {errorMessage}
+    <div className="">
+      <form
+        onSubmit={onSubmitHandler}
+        className="form signup-container"
+        autoComplete="off"
+      >
+        <h2>Signup</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={formData.username}
+          onChange={onChangeHandler}
+          name="username"
+          required="required"
+        />
+        <input
+          type="password"
+          placeholder="Create password"
+          value={formData.password}
+          onChange={onChangeHandler}
+          name="password"
+          required="required"
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          value={formData.confirmPassword}
+          onChange={onChangeHandler}
+          name="confirmPassword"
+          required="required"
+        />
+        {errorMessage ? (
+          <p className="" id="error-message">
+            {errorMessage}
+          </p>
+        ) : (
+          <></>
+        )}
+        {sucessMessage ? (
+          <p className="" id="success-message">
+            {sucessMessage}
+          </p>
+        ) : (
+          <></>
+        )}
+        <button type="submit">Signup</button>
+        <p>
+          Already have an account?{" "}
+          <span onClick={() => changeLogin(true)}>Login</span>
         </p>
-      ) : (
-        <></>
-      )}
-      {sucessMessage ? (
-        <p className="" id="success-message">
-          {sucessMessage}
-        </p>
-      ) : (
-        <></>
-      )}
-      <button onClick={handleSignup}>Signup</button>
-      <p>
-        Already have an account?{" "}
-        <span onClick={() => changeLogin(true)}>Login</span>
-      </p>
-      <SocailLogin />
+        <SocailLogin />
+      </form>
     </div>
   );
 };
